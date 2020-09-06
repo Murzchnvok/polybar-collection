b88c86a06775a58cdb85424ae2a7d2904fd4736f
Pedro Araujo <murzchnvok@gmail.com>
2020-09-06 20:45:16 -0300

    updated

--- scripts/weather/weather.py
+++ scripts/weather/weather.py
@@ -1,19 +1,40 @@
+import argparse
 import json
 import urllib.request
 
-# TODO: add argparse for better usage
-# TODO: funtion for wttr
-# http://wttr.in/?format=j1
-
-# get your own api key https://openweathermap.org/api
+# This API KEY is for my personal usage.
+# Please get your own API KEY here https://openweathermap.org/api,
+# because the number of request per day is limited.
 OPENWEATHER_API_KEY = "970606528befaa317698cc75083db8b2"
 OPENWEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
 
+USAGE_MESSAGE = """%(prog)s [-c [CITY_NAME]] [-u [UNIT]] [-a [API_KEY]]
+No argument(s) the program will get the city name and use metric unit temperature
 
-# Temperature in Kelvin is used by default.
-# For temperature in Fahrenheit: imperial
-# For temperature in Celsius: metric
-def openweather(city_name, units="", api_key=OPENWEATHER_API_KEY):
+Some examples:
+\t%(prog)s
+\t%(prog)s -c london
+\t%(prog)s -u imperial
+\t%(prog)s -c florida -u imperial
+\t%(prog)s -c "rio de janeiro" -u metric -a 439d4b804bc8187953eb36d2a8c26a02
+"""
+
+
+def getip():
+    request = urllib.request.urlopen("https://api.ipify.org?format=json")
+    if request.getcode() == 200:
+        data = json.loads(request.read())
+        return data["ip"]
+
+
+def getcity():
+    request = urllib.request.urlopen("http://ip-api.com/json/" + getip())
+    if request.getcode() == 200:
+        data = json.loads(request.read())
+        return data["city"]
+
+
+def openweather(city_name, units, api_key):
     request = urllib.request.urlopen(
         f"{OPENWEATHER_URL}?q={city_name.replace(' ', '+')}&units={units}&appid={api_key}"
     )
@@ -47,4 +68,28 @@ def openweather(city_name, units="", api_key=OPENWEATHER_API_KEY):
         print(f"E: {request.getcode()}")
 
 
-openweather("aracaju", units="metric")
+parser = argparse.ArgumentParser(
+    usage=USAGE_MESSAGE, description="Display information about the weather.",
+)
+parser.add_argument(
+    "-c", metavar="CITY", dest="city", type=str, nargs=1, help="city name",
+)
+parser.add_argument(
+    "-u",
+    metavar="UNIT",
+    dest="unit",
+    type=str,
+    nargs=1,
+    help="unit of temperature (metric, imperial)",
+)
+parser.add_argument(
+    "-a", metavar="API_KEY", dest="api_key", nargs=1, help="API Key",
+)
+
+args = parser.parse_args()
+
+openweather(
+    args.city[0] if args.city else getcity(),
+    args.unit[0] if args.unit else "metric",
+    args.api_key[0] if args.api_key else OPENWEATHER_API_KEY,
+)
