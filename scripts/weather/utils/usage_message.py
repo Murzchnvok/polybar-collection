cd52202a5e3e0a17490a34d44014bf8fb0554723
Murzchnvok <murzchnvok@gmail.com>
2020-12-15 07:52:08 -0300

    updated

--- scripts/weather/utils/usage_message.py
+++ scripts/weather/utils/usage_message.py
@@ -7,7 +7,7 @@ except:
     print("Run: pip3 install colored --user")
     sys.exit(0)
 
-USAGE_MESSAGE = f"""{attr(1)}{fg(2)}%(prog)s {fg(3)}[-c [CITY_NAME]] [-u [UNIT]] [-a [API_KEY]] {fg(4)}[-v] [--color]{attr(0)}
+USAGE_MESSAGE = f"""{attr(1)}{fg(2)}%(prog)s {fg(3)}[-c [CITY_NAME]] [-u [UNIT]] [-a [API_KEY]] {fg(4)}[-v]{attr(0)}
 
 Some examples:
 ~$ {attr(1)}{fg(2)}%(prog)s{attr(0)}
@@ -22,8 +22,8 @@ Some examples:
 ~$ {attr(1)}{fg(2)}%(prog)s {fg(4)}-v -C{fg(3)} -u metric{attr(0)}
 ::> {attr(1)}{fg(4)}26ºC{attr(0)}, {fg(5)}Broken Clouds{attr(0)}
 
-~$ {attr(1)}{fg(2)}%(prog)s {fg(3)}-c florida -u metric {fg(4)}-v --color{attr(0)}
-::> {attr(1)}{fg(4)}27ºC{attr(0)}, {fg(5)}Thunderstorm{attr(0)}
+~$ {attr(1)}{fg(2)}%(prog)s {fg(3)}-c florida -u metric {fg(4)}-v{attr(0)}
+::> 27ºC, Thunderstorm
 
 ~$ {attr(1)}{fg(2)}%(prog)s {fg(3)}-c "rio de janeiro" -u metric -a 439d4b804bc8187953eb36d2a8c26a02 {fg(4)}-v{attr(0)}
 ::> 25ºC
