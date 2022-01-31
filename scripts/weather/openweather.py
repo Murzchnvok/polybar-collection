5ad23fb979648334c194c28ee70658cff76addee
Murzchnvok <murzchnvok@gmail.com>
2022-01-31 10:32:42 -0300

    added headers to get request

--- scripts/weather/openweather.py
+++ scripts/weather/openweather.py
@@ -8,7 +8,8 @@ OPENWEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
 def get_weather(city: str, lang: str, unit: str, api_key: str) -> dict[str, str] | None:
     try:
         r = requests.get(
-            f"{OPENWEATHER_URL}?q={city}&lang={lang}&units={unit}&appid={api_key}"
+            f"{OPENWEATHER_URL}?q={city}&lang={lang}&units={unit}&appid={api_key}",
+            headers={"User-agent": "Mozilla/5.0"},
         )
         data = r.json()
         temp = data.get("main").get("temp")
